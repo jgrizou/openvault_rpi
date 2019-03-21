16e582c98b6a2c763e862f71d0222cb0f9847549
Jonathan Grizou <jonathan.grizou@gmail.com>
2019-03-21 19:10:26 +0100

    make ocompatible with MacOs

--- app/server/lock_tools.py
+++ app/server/lock_tools.py
@@ -6,16 +6,25 @@ HERE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe
 
 import time
 
-import RPi.GPIO as GPIO
+# try catch so we can develop away from a RPI
+try:
+    import RPi.GPIO as GPIO
 
+    LOCK_PIN = 21
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(LOCK_PIN, GPIO.OUT)
 
-LOCK_PIN = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(LOCK_PIN, GPIO.OUT)
+    def open():
+        GPIO.output(LOCK_PIN, GPIO.HIGH)
 
-def open():
-    GPIO.output(LOCK_PIN, GPIO.HIGH)
+    def close():
+        GPIO.output(LOCK_PIN, GPIO.LOW)
 
-def close():
-    GPIO.output(LOCK_PIN, GPIO.LOW)
+except:
+
+    def open():
+        print('Opening Vault')
+
+    def close():
+        print('Closing Vault')
